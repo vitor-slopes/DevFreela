86ca7636dda998afbd1bc546e50a4430440d5f99
Vitor Lopes <vitor_slopes@hotmail.com>
2024-06-10 22:40:02 -0300

    continuei a criação da camada Aplication

---
+++ DevFreela.Aplication/Services/Interfaces/IProjectService.cs
@@ -0,0 +1,23 @@
+﻿using DevFreela.Aplication.InputModels;
+using DevFreela.Aplication.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DevFreela.Aplication.Services.Interfaces
+{
+    public interface IProjectService
+    {
+        List<ProjectViewModel> GetAll(string query);
+        ProjectDetailsViewModel GetById(int id);
+        int Create(NewProjectInputModel inputModel);
+        void Update(UpdateProjectInputModel inputModel);
+        void Delete(int id);
+        void CreateComment(CreateCommentInputModel inputModel);
+        void Start(int id);
+        void Finish(int id);
+    }
+}
---
+++ Devfreela.Infrastructure/Persistence/DevFreelaDbContext.cs
@@ -0,0 +1,41 @@
+﻿using DevFreela.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Devfreela.Infrastructure.Persistence
+{
+    public class DevFreelaDbContext
+    {
+        public DevFreelaDbContext()
+        {
+            Projects = new List<Project>
+            {
+                new Project("Meu projeto ASPNET Core1", "Minha descrição de projeto1", 1, 1, 10000),
+                new Project("Meu projeto ASPNET Core2", "Minha descrição de projeto2", 1, 1, 20000),
+                new Project("Meu projeto ASPNET Core3", "Minha descrição de projeto3", 1, 1, 30000)
+
+            };
+
+            Users = new List<User>
+            {
+                new User("Vitor", "emai@vitor.com",new DateTime(1985, 6, 1)),
+                new User("Rafael", "emai@rafael.com",new DateTime(1981, 2, 17)),
+                new User("Lucia", "emai@lucia.com",new DateTime(1954, 3, 22))
+            };
+
+            Skills = new List<Skill>
+            {
+                new Skill(".NET"),
+                new Skill("C#"),
+                new Skill("SQL")
+            };
+        }
+        public List<Project> Projects { get; private set; }
+        public List<User> Users{ get; private set; }
+        public List<Skill> Skills { get; private set; }
+        public List<ProjectComment> ProjectComments { get; set; }
+    }
+}
