e3436c35be6ede2da9c6b3884c7943da03daa482
Vitor Lopes <vitor_slopes@hotmail.com>
2024-07-25 14:22:22 -0300

    Adiciona camada de interface e viewmodel de skills

--- DevFreela.Aplication/ViewModels/ProjectViewModel.cs
+++ DevFreela.Aplication/ViewModels/ProjectViewModel.cs
@@ -8,12 +8,14 @@ namespace DevFreela.Aplication.ViewModels
 {
     public class ProjectViewModel
     {
-        public ProjectViewModel(string title, DateTime createdAt)
+        public ProjectViewModel(int id, string title, DateTime createdAt)
         {
+            Id = id;
             Title = title;
             CreatedAt = createdAt;
         }
 
+        public int Id { get; set; }
         public string Title { get; private set; }
         public DateTime CreatedAt { get; private set; } 
     }
